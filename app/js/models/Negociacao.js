43f546963dcefee2cdb52e6e3b0767ced8e1c304
Leonardo Gomes <leonardogomes2704@gmail.com>
2018-10-28 22:39:12 -0300

    Tipando e fazendo cast

--- app/js/models/Negociacao.js
+++ app/js/models/Negociacao.js
@@ -1,24 +1,19 @@
-class Negociacao{
-
-    constructor(data, quantidade, valor){
-        this._data = data;
-        this._quantidade = quantidade;
-        this._valor = valor;
+class Negociacao {
+    constructor(_data, _quantidade, _valor) {
+        this._data = _data;
+        this._quantidade = _quantidade;
+        this._valor = _valor;
     }
-
-    get data(){
+    get data() {
         return this._data;
     }
-
-    get quantidade(){
+    get quantidade() {
         return this._quantidade;
     }
-
-    get valor(){
+    get valor() {
         return this._valor;
     }
-
-    get volume(){
+    get volume() {
         return this._quantidade * this._valor;
     }
 }
--- app/js/models/app.js
+++ app/js/models/app.js
@@ -1,3 +1,2 @@
-const negociacao =  new Negociacao(new Date(), 1, 100);
-console.log(negociacao);
-console.log(negociacao.volume);
+const controller = new NegociacaoController();
+document.querySelector('form').addEventListener('submit', controller.adciona.bind(controller));
